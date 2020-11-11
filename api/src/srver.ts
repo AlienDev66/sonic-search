4526641da4a3d9b05334393258fd0280e72b220d
AlienDev66 <p.capitango65@gmail.com>
2020-11-11 20:48:38 +0100

    :sparkles: Getting Started.

---
+++ api/src/srver.ts
@@ -0,0 +1,23 @@
+import express from "express";
+
+const app = express();
+
+app.post("/pages", (request, response) => {
+  // Slasvando no banco de dados
+
+  return response.status(2001).send();
+});
+
+app.get("/search", (request, response) => {
+  // Slasvando no banco de dados
+
+  return response.json([]);
+});
+
+app.get("/suggest", (request, response) => {
+  // Slasvando no banco de dados
+
+  return response.status(2001).send();
+});
+
+app.listen(3005);
